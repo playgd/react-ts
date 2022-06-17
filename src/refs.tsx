5b6a80cf58cbd995bbcaab39bad407b52cf75346
Fernando Daciuk <f.daciuk@gmail.com>
2022-06-17 08:31:51 -0300

    feat: ref for inputs

--- src/refs.tsx
+++ src/refs.tsx
@@ -1,36 +1,18 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef, FormEvent } from 'react'
 
 export function Refs () {
-  const [counter, setCounter] = useState(0)
-  const semRef = useRef(0)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  console.log('canvasRef:', canvasRef)
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  useEffect(() => {
-    handleCanvas()
-  }, [])
-
-  useEffect(() => {
-    console.log('antes de mudar o semRef:', semRef.current)
-    semRef.current++
-    console.log('depois de mudar o semRef:', semRef.current)
-  }, [counter, semRef])
-
-  const handleCanvas = () => {
-    const canvas = canvasRef.current
-    if (!canvas) return
-
-    const ctx = canvas.getContext('2d')
-
-    if (!ctx) return
-    ctx.fillStyle = 'green'
-    ctx.fillRect(10, 10, 150, 100)
+  const handleInput = (e: FormEvent<HTMLInputElement>) => {
+    console.log('e.currentTarget.value', e.currentTarget.value)
+    if (!inputRef.current) return
+    inputRef.current.value = inputRef.current?.value.replace(/\D+/g, '')
+    // e.currentTarget.value = e.currentTarget.value.replace(/\D+/g, '')
   }
 
   return (
     <>
-      <h1 onClick={() => setCounter(c => c + 1)}>Refs: {counter}</h1>
-      <canvas ref={canvasRef} data-js='canvas' style={{ border: '1px solid #000' }} />
+      <input ref={inputRef} onInput={handleInput} />
     </>
   )
 }
