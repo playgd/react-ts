a210a3662919d07f2330051ca72ba43d2f5fdd4d
Fernando Daciuk <f.daciuk@gmail.com>
2022-06-30 08:41:00 -0300

    feat: async code with userReducer

--- src/redux.tsx
+++ src/redux.tsx
@@ -1,121 +1,166 @@
 import { useReducer } from 'react'
 
-interface MovieState {
-  id: number
-  name: string
+type UserData = { id: number, name: string }
+type UserError = string
+
+type UserStateIdle = {
+  status: 'IDLE'
+  data: null
+  error: null
 }
 
-type MoviesState = MovieState[]
-
-interface MoviesActionAdd {
-  type: 'ADD'
-  payload: {
-    name: string
-  }
+type UserStateProcessing = {
+  status: 'PROCESSING'
 }
 
-interface MoviesActionRemove {
-  type: 'REMOVE'
-  payload: {
-    id: number
-  }
+type UserStateSuccess = {
+  status: 'SUCCESS'
+  data: UserData
 }
 
-type MoviesAction = MoviesActionAdd | MoviesActionRemove
-
-type State = number
-
-interface MainState {
-  counter: State,
-  movies: MoviesState,
+type UserStateError = {
+  status: 'ERROR'
+  error: UserError
 }
 
-interface CounterAction {
-  type: 'INCREMENT' | 'DECREMENT'
+type UserState =
+  | UserStateIdle
+  | UserStateProcessing
+  | UserStateSuccess
+  | UserStateError
+
+const initialState: UserState = {
+  status: 'IDLE',
+  data: null,
+  error: null,
 }
 
-type MainAction<C, M> = C | M
-
-const initialState: State = 0
-const initialMoviesState: MoviesState = []
-
-function mainReducer <C = CounterAction, M = MoviesAction> (state: MainState, action: MainAction<C, M>) {
-  return {
-    counter: counterReducer(state.counter, action),
-    movies: moviesReducer(state.movies, action),
-  }
+type UserActionsIdle = {
+  type: 'IDLE'
 }
 
-const mainInitialState = {
-  counter: initialState,
-  movies: initialMoviesState,
+type UserActionsProcessing = {
+  type: 'PROCESSING'
 }
+
+type UserActionsSuccess = {
+  type: 'SUCCESS'
+  data: UserData
+}
+
+type UserActionsError = {
+  type: 'ERROR'
+  error: UserError
+}
+
+type UserActions =
+  | UserActionsIdle
+  | UserActionsProcessing
+  | UserActionsSuccess
+  | UserActionsError
 
 export function Redux () {
-  // const [counter, dispatchCounter] = useReducer(counterReducer, initialState)
-  // const [movies, dispatchMovies] = useReducer(moviesReducer, initialMoviesState)
-  const [store, dispatch] = useReducer(mainReducer, mainInitialState)
+  const [user, dispatch] = useReducer(userReducer, initialState)
+  console.log('user:', user)
+
+  const handleClick = async () => {
+    dispatch({ type: 'PROCESSING' })
+    try {
+      const userData = await getUser(123)
+      console.log('userData (retorno da API):', userData)
+      if ('id' in userData) {
+        dispatch({ type: 'SUCCESS', data: userData })
+        return
+      }
+
+      dispatch({ type: 'IDLE' })
+    } catch (e) {
+      if (e instanceof Error) {
+        dispatch({ type: 'ERROR', error: e.message })
+      }
+    }
+  }
 
   return (
     <>
-      <h1 onClick={() => dispatch({ type: 'INCREMENT' })}>Redux: {store.counter}</h1>
-      <ul>
-        {store.movies.map(movie => (
-          <li key={movie.id}>
-            {movie.name}
-            <button onClick={() => dispatch(remove(movie.id))}>
-              Remover
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={() => dispatch(add('New movie' + Math.random()))}>
-        +
-      </button>
+      <h1>Redux</h1>
+      <button onClick={handleClick}>Buscar dados do usuário</button>
+      {user.status === 'PROCESSING' && <h3>Carregando dados do usuário...</h3>}
+      {user.status === 'SUCCESS' && <UserDetails user={user.data} />}
+      {user.status === 'ERROR' && <h3>Deu erro! {user.error}</h3>}
     </>
   )
 }
 
+type UserDetailsProps = {
+  user: UserData
+}
+function UserDetails ({ user }: UserDetailsProps) {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>UserID: {user.id}</p>
+    </div>
+  )
+}
 
-function counterReducer (state: State, action: CounterAction): State {
-  if (action.type === 'DECREMENT') {
-    return state - 1
+function userReducer (state: UserState, action: UserActions) {
+  if (action.type === 'PROCESSING') {
+    return {
+      ...state, // retorno tudo ou zero os valores?
+      status: action.type,
+    }
   }
 
-  if (action.type === 'INCREMENT') {
-    return state + 1
+  if (action.type === 'SUCCESS') {
+    return {
+      status: action.type,
+      data: action.data,
+      error: null,
+    }
+  }
+
+  if (action.type === 'IDLE') {
+    return {
+      status: action.type,
+      data: null,
+      error: null,
+    }
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      status: action.type,
+      data: null,
+      error: action.error,
+    }
   }
 
   return state
 }
 
-function moviesReducer (
-  state: MoviesState = initialMoviesState,
-  action: MoviesAction,
-): MoviesState {
-  if (action.type === 'ADD') {
-    return state.concat({
-      id: state.length,
-      name: action.payload.name,
-    })
-  }
-
-  if (action.type === 'REMOVE') {
-    return state.filter(movie => movie.id !== action.payload.id)
-  }
-  return state
+// API que conversa com backend
+interface ApiResponseSuccess {
+  success: true
+  id: number
+  name: string
 }
 
-function add (name: string): MoviesActionAdd {
-  return {
-    type: 'ADD',
-    payload: { name },
-  }
-}
+type ApiResponseError = Error
+type ApiResponse = ApiResponseSuccess | ApiResponseError
 
-function remove (id: number): MoviesActionRemove {
-  return {
-    type: 'REMOVE',
-    payload: { id },
-  }
+function getUser (id: number): Promise<ApiResponse> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.ceil(Math.random() * 2) === 1) {
+        return reject(new Error('Não foi possível encontrar dados do usuário'))
+      }
+
+      resolve({
+        success: true,
+        id,
+        name: 'John Doe ' + Math.random() * id,
+      })
+    }, 2000)
+  })
 }
