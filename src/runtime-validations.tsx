8efefe20a529ca418f0c523ffc82841c112812d7
Fernando Daciuk <f.daciuk@gmail.com>
2022-07-07 08:02:12 -0300

    feat: show how to deal with JSON.parse

--- src/runtime-validations.tsx
+++ src/runtime-validations.tsx
@@ -1,14 +1,38 @@
 import { useEffect } from 'react'
-import { getUser } from '@/api/get-users'
+import { z } from 'zod'
+
+const userSchema = z.object({
+  userName: z.string(),
+  age: z.number(),
+  street: z.string(),
+})
+
+// type User = z.infer<typeof userSchema>
 
 export function RuntimeValidations () {
   useEffect(() => {
-    getUser('fdaciuk')
-      .then(user => console.log('user:', user))
-      .catch(error => console.log('error:', error))
+    getUser().then(result => {
+      console.log('retorno da api:', result)
+      const data = userSchema.safeParse(JSON.parse(result))
+      if (data.success === false) {
+        console.log('deu erro:', data.error)
+        return
+      }
+
+      console.log(data.data.userName, data.data.age, data.data.street)
+    })
   }, [])
 
   return (
     <h1>RuntimeValidations</h1>
   )
+}
+
+async function getUser () {
+  // código do backend
+  return JSON.stringify({
+    userName: 'Daciuk',
+    age: 37,
+    street: 'Rua dos bobos',
+  })
 }
